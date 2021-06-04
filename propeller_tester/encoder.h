dd3c9baea0f3978d71b24f76fcba0f69bf091167
Savo <56771837+savob@users.noreply.github.com>
2021-06-04 15:21:11 -0400

    Added physical constants for shaft

--- propeller_tester/encoder.h
+++ propeller_tester/encoder.h
@@ -1,14 +1,19 @@
 #include <EEPROM.h> //Needed to record user settings
 
-// Encoder pins
-const byte motorEnc = 2;
-const byte propEnc = 3;
+// Shaft Constants
+const float Jm = 57.9402017;    // J * Shear Modulus (Nm^2)
+const float shaftLength = 0.6;  // Shaft length in meters
+// Jm needs to be precalculated since using a large range of numbers can lead
+// to inacccurate results on microcontrollers. J is generally on the order of
+// 10^10 while J is 10^-9, shaft length and phase are around 10^0.
 
-// Constants
-unsigned long steadyStatePhaseDif = 0; // Radians times 1000, set once in setup
-const byte numberTicks = 2; // Number of encoder pulses per rotation
-const unsigned int holdOffPeriod = 10000; // Holdoff after successive events
-const int PHASE_DIFF_LOCATION = 20; // Location of phase difference in EEPROM
+// Encoder constants
+const byte motorEnc = 2;                // Pin for motor side encoder
+const byte propEnc = 3;                 // Pin for propeller end encoder
+unsigned long steadyStatePhaseDif = 0;  // Radians times 1000, set once in setup
+const int PHASE_DIFF_LOCATION = 20;     // Location of phase difference in EEPROM
+const byte numberTicks = 2;             // Number of encoder pulses per rotation
+const unsigned int holdOffPeriod = 10000; // Holdoff after successive events (us)
 
 // Variables used in interrupts
 volatile unsigned long motorSideLast, propSideLast;
@@ -18,3 +23,4 @@ volatile float phase; // Phase in radians
 void setupEncoders();
 void motorInterrupt();
 void propInterrupt();
+float findTorque();
